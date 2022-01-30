794068ff1b1866c5f8ed74ef4efcdaaa258b4f6a
lee dong hyun <donghyun.lee@fetching.co.kr>
2022-01-30 16:11:57 +0900

    favicon 개선

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -9,80 +9,30 @@ const SEO: FC<Props> = ({ title, image, description }) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <link
         rel="apple-touch-icon"
-        sizes="57x57"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="apple-touch-icon"
         sizes="180x180"
-        href="/images/assets/brand.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/images/assets/brand.png"
+        href="/apple-touch-icon.png?v=1"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/images/assets/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/images/assets/favicon-96x96.png"
+        href="/favicon-32x32.png?v=1"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/images/assets/favicon-96x96.png"
+        href="/favicon-16x16.png?v=1"
       />
-      <link rel="manifest" href="/images/assets/manifest.json" />
+      <link rel="manifest" href="/site.webmanifest?v=1" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#000000" />
+      <link rel="shortcut icon" href="/favicon.ico?v=1" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
       <meta name="theme-color" content="#ffffff" />
     </Head>
   );
